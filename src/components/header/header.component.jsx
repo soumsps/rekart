118cf3ae0a40090a726e2ccb342f464e34ef4758
Soumendu Prasad Sinha <soumsps@gmail.com>
2019-11-18 12:37:53 +0530

    added signin page and sign in component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,6 +14,9 @@ const Header = () => (
       <Link className='option' to='/shop'>
         SHOP
       </Link>
+      <Link className='option' to='/signin'>
+        SIGN IN
+      </Link>
       <Link className='option' to='/contact'>
         CONTACT
       </Link>
