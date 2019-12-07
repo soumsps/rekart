579a924fb2d448937e5662c1350c6abcd1faef1d
Soumendu Prasad Sinha <soumsps@gmail.com>
2019-12-07 19:06:29 +0530

    optimizing build for heroku, turning off logging in production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+// logging will be working on development builds only
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
