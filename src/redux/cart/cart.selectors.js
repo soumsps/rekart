a450522cc400c9ec0b4771a2058eb44683be665e
Soumendu Prasad Sinha <soumsps@gmail.com>
2019-12-09 16:09:24 +0530

    updating our app with reselect library to memoize cartItems and cartItems Quantity count

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+
+// we are doing step wise selection of cat items and memoizing it
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
