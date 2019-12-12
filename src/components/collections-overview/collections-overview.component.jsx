b9dea889bf8bc9847a6bec4d13c496af6349738d
Soumendu Prasad Sinha <soumsps@gmail.com>
2019-12-12 16:11:52 +0530

    moved directory and shop data into its own reducer, and also added collection page

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
 import './collections-overview.styles.scss';
@@ -19,7 +19,7 @@ const CollectionOverview = ({ collections }) => {
 
 const mapStateToProps = state =>
   createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
   });
 
 export default connect(mapStateToProps)(CollectionOverview);
